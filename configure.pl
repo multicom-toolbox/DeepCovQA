1159146adbee8b77441165a92338cbd1b29119fc
jh7x3 <jh7x3@mail.missouri.edu>
2018-09-10 21:14:50 -0500

    update files

--- configure.pl
+++ configure.pl
@@ -3,18 +3,20 @@
  use Cwd;
  use Cwd 'abs_path';
 
-######################## !!! customize settings here !!! ############################
-#																					#
-# Set installation directory of DeepCovQA to your unzipped DeepCovQA directory            #
-     
- $install_dir = "/your_path/DeepCovQA";
-######################## !!! End of customize settings !!! ##########################
 
-if($install_dir eq "/your_path/DeepCovQA")
-{# user forgets to set the default path of DeepCovQA, try to solve this problem
-    $install_dir = getcwd;
-    $install_dir=abs_path($install_dir);
-}
+######################## !!! customize settings here !!! ############################
+#				
+
+$R_SCRIPT="/home/jh7x3/tools/R-3.1.1/bin/Rscript";
+-f $R_SCRIPT || die "$R_SCRIPT file doesn't exist.\n";
+
+
+
+# Set installation directory of DeepCovQA to your unzipped DeepCovQA directory            #
+
+$install_dir = getcwd;
+$install_dir=abs_path($install_dir);
+
 
 
 if(!-s $install_dir)
@@ -57,7 +59,7 @@ if (prompt_yn("DeepCovQA will be installed into <$install_dir> ")){
 print "Start install DeepCovQA into <$install_dir>\n";
 
 
-$files		="scripts/run_DeepCovQA.pl,scripts/P1_feature_generation_parallel.pl,scripts/run_ProQ3_model_local.sh,tools/proq3/paths.sh,tools/proq3/bin/run_all_external.pl,tools/proq3/apps/psipred25/runpsipred,tools/predisorder1.1/configure.pl";
+$files		="scripts/run_DeepCovQA.pl,scripts/P1_feature_generation_parallel.pl,scripts/run_ProQ3_model_local.sh,tools/proq3/paths.sh,tools/proq3/bin/run_all_external.pl,tools/predisorder1.1/configure.pl,scripts/DN_package/predict_score.py";
 
 @updatelist		=split(/,/,$files);
 
@@ -78,6 +80,72 @@ foreach my $file (@updatelist) {
 			print "Current ".$line."\n";
 			print "Change to ".substr($line,0,index($line, '=')+1)." \'".$install_dir."\';\n\n\n";
 			print OUT substr($line,0,index($line, '=')+1)."\'".$install_dir."\';\n";
+		}else{
+			print OUT $line."\n";
+		}
+	}
+	close IN;
+	close OUT;
+	system("mv $file2update.tmp $file2update");
+	system("chmod 755  $file2update");
+
+
+}
+
+$files		="tools/proq3/apps/psipred25/runpsipred";
+
+@updatelist		=split(/,/,$files);
+
+foreach my $file (@updatelist) {
+	$file2update=$install_dir.$file;
+	
+	$check_log ='GLOBAL_PATH=';
+	open(IN,$file2update) || die "Failed to open file $file2update\n";
+	open(OUT,">$file2update.tmp") || die "Failed to open file $file2update.tmp\n";
+	while(<IN>)
+	{
+		$line = $_;
+		chomp $line;
+
+		if(index($line,$check_log)>=0)
+		{
+			print $file2update."\n";
+			print "Current ".$line."\n";
+			print "Change to ".substr($line,0,index($line, '=')+1).$install_dir."\n\n\n";
+			print OUT substr($line,0,index($line, '=')+1).$install_dir."\n";
+		}else{
+			print OUT $line."\n";
+		}
+	}
+	close IN;
+	close OUT;
+	system("mv $file2update.tmp $file2update");
+	system("chmod 755  $file2update");
+
+
+}
+
+$files		="tools/proq3/paths.sh";
+
+@updatelist		=split(/,/,$files);
+
+foreach my $file (@updatelist) {
+	$file2update=$install_dir.$file;
+	
+	$check_log ='R_SCRIPT=';
+	open(IN,$file2update) || die "Failed to open file $file2update\n";
+	open(OUT,">$file2update.tmp") || die "Failed to open file $file2update.tmp\n";
+	while(<IN>)
+	{
+		$line = $_;
+		chomp $line;
+
+		if(index($line,$check_log)>=0)
+		{
+			print $file2update."\n";
+			print "Current ".$line."\n";
+			print "Change to ".substr($line,0,index($line, '=')+1).$R_SCRIPT."\n\n\n";
+			print OUT substr($line,0,index($line, '=')+1).$R_SCRIPT."\n";
 		}else{
 			print OUT $line."\n";
 		}
@@ -117,9 +185,23 @@ if(-f 'configure.pl')
 }else{
 	die "The configure.pl file for sspro doesn't exist, please contact us(Jie Hou: jh7x3\@mail.missouri.edu)\n";
 }
+print "\n#########  Setting up SCRATCH \n";
+$ssprodir = $install_dir.'/tools/SCRATCH-1D_1.1/';
+chdir $ssprodir;
+if(-f 'install.pl')
+{
+	$status = system("perl install.pl");
+	if($status){
+		die "Failed to run perl install.pl \n";
+		exit(-1);
+	}
+}else{
+	die "The configure.pl file for $ssprodir doesn't exist, please contact us(Jie Hou: jh7x3\@mail.missouri.edu)\n";
+}
 
 
-
+`rm -rf $install_dir/tools/proq3/apps/sspro4`;
+`rm -rf $install_dir/tools/proq3/apps/blast-2.2.26`;
 print "\n#########  Setting up proq3 \n";
 symlink("$install_dir/tools/DeepQA/tools/sspro4/","$install_dir/tools/proq3/apps/sspro4");
 symlink("$install_dir/tools/blast-2.2.26/","$install_dir/tools/proq3/apps/blast-2.2.26");
